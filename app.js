4373bff81442418321a72edffc510dd67c98d591
CarlosHenrique2208 <85198581+CarlosHenrique2208@users.noreply.github.com>
2021-10-20 12:43:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// CODIGO FEITO NA AULA 19, NÃO FUNCIONOU
 const form = document.querySelector('form')
 
 const correctAnswers = ['B', 'B', 'B', 'B',]
@@ -20,5 +21,33 @@ form.addEventListener('submit', event => {
             score += 25
         }
     });
+    console.log(score)
+})
+
+
+// REVISADO DEPOIS DA ATIVIDADE 20, FUNCIONA
+const form = document.querySelector('.quiz-form')
+
+const correctAnswers = ['B', 'B', 'B', 'B', 'A', 'B']
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    
+    let score = 0
+    
+    const userAnswers = [
+        form.inputQuestion1.value,
+        form.inputQuestion2.value,
+        form.inputQuestion3.value,
+        form.inputQuestion4.value,
+        form.inputQuestion5.value,
+        form.inputQuestion6.value
+    ]
+
+    userAnswers.forEach((userAnswer, index) => {
+        if(userAnswer === correctAnswers[index]){
+            score+= 25
+        }
+    })
     console.log(score)
 })
